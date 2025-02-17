94f253c791a88183d546678761aa3e189f7cd45a
juhanamb <vandaali@gmail.com>
2025-02-17 17:49:47 +0200

    Added main.cpp

---
+++ main.cpp/main.cpp.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  // Include all libraries 
+  // Scan for I2c devices. 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  /*
+  In loop (need to find appropriate timings):
+  Update screen
+  Read Sensors: HR, temperature, IMU
+  Calculate values for display
+  */
+  
+}
